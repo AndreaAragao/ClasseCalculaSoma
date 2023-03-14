db2bbe11332e7cbeda29d80bf7d5c4ce5f57add0
Andréa <andreamaragao@gmail.com>
2023-03-13 22:44:01 -0300

    Adicionar arquivos de projeto.

---
+++ App/Calculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projetos08.App
+{
+    internal class Calculo
+    {
+        public int PrimeiroValor { get; set; }
+        public int SegundoValor { get; set; }
+        public int TerceiroValor { get; set; }
+
+        public Calculo(int primeirovalor, int segundovalor, int terceirovalor)
+        {
+            PrimeiroValor = primeirovalor;
+            SegundoValor = segundovalor;   
+            TerceiroValor = terceirovalor;
+        }
+        
+        public int calcularSoma()
+        {
+            return (PrimeiroValor + SegundoValor + TerceiroValor);
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Projetos08.App;
+using System;
+
+namespace Projetos08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Olá usuário, vamos brincar? Você irá digitar três valores (inteiros):\n");
+            //int.Parse(Console.ReadLine());
+            
+            int primeiroValor, segundoValor, terceiroValor;
+            
+            Console.WriteLine("Digite o primeiro valor:");
+            primeiroValor = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDigite o segundo valor:");
+            segundoValor = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDigite o terceiro valor:");
+            terceiroValor = int.Parse(Console.ReadLine());
+            
+            Calculo calculo = new Calculo(primeiroValor, segundoValor, terceiroValor);
+
+                Console.WriteLine("\nO resultado é: " + calculo.calcularSoma() + ".");
+         }
+    }
+}
